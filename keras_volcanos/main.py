70a827a07cd78c503c3d1afaaeaa055107af7636
Will Gerecke <wlgfour@gmail.com>
2018-11-03 21:29:25 -0400

    made partially untested changes to file management and worked on image collection during training. changed to once an epoch

--- keras_volcanos/main.py
+++ keras_volcanos/main.py
@@ -28,13 +28,12 @@ class Debug(object):
         if debug:
             self._flags.append('debug')  # if DEBUG and 'debug' means debugging
             print('----------DEBUGGING----------')
+
     def __add__(self, other: str):
         # returns false if debug is on and  flag in flags
         # true if debug is off or other not in flags
-        print(self._flags)
         debugging = 'debug' in self._flags
         flag = other in self._flags
-        print(debugging, flag)
         return not(debugging and flag)
 
 
@@ -53,7 +52,7 @@ def show_imgs(imgs, labels, rows, cols):
 
 # globals
 print('defining globals')
-DEBUG = Debug(True, 'load_data', 'train')
+DEBUG = Debug(False, 'load_data', 'train')
 VISUALIZE = False
 SAVE = True
 LOAD = False
@@ -61,22 +60,34 @@ MODEL_NUMBER = 'v2.2_images'
 BASE_DIR = f'./log_dir/{MODEL_NUMBER}'
 SAVE_FILE = f'{BASE_DIR}/volcano_classifier_{MODEL_NUMBER}.h5'
 
-# tensorboard
+# tensorboard and file management
+print('--starting file management--')
 TBOARD_DIR = f'{BASE_DIR}/tboard_{MODEL_NUMBER}'
 TBOARD_CUR_DIR = f'{TBOARD_DIR}/current'
 TBOARD_HIST_DIR = f'{TBOARD_DIR}/history'
-if os.path.isdir(BASE_DIR):
-    if os.path.exists(SAVE_FILE):
-        LOAD = True
-    if os.path.isdir(TBOARD_DIR):
-        # move past logfiles to history directory
-        if not os.path.isdir(TBOARD_HIST_DIR):
-            os.mkdir(TBOARD_HIST_DIR)
-        if not os.path.isdir(TBOARD_CUR_DIR):
-            os.mkdir(TBOARD_CUR_DIR)
-            log_files = [f for f in os.listdir(TBOARD_CUR_DIR) if os.path.isfile(f'{TBOARD_CUR_DIR}/{f}')]
-            for file in log_files:
-                os.rename(f'{TBOARD_CUR_DIR}/{file}', f'{TBOARD_HIST_DIR}/{file}')
+if not os.path.isdir(BASE_DIR):
+    print(f'    making model {MODEL_NUMBER} log directory')
+    os.mkdir(BASE_DIR)
+if not os.path.isdir(TBOARD_DIR):
+    print('    making tensorboard log directory')
+    os.mkdir(TBOARD_DIR)
+# move past logfiles to history directory
+if not os.path.isdir(TBOARD_HIST_DIR):
+    print('    making tensorboard history directory')
+    os.mkdir(TBOARD_HIST_DIR)
+if not os.path.isdir(TBOARD_CUR_DIR):
+    print('    making tensorboard current directory')
+    os.mkdir(TBOARD_CUR_DIR)
+print('    moving tensorboard log files from current directory to history')
+log_files = [f for f in os.listdir(TBOARD_CUR_DIR) if os.path.isfile(f'{TBOARD_CUR_DIR}/{f}')]
+for file in log_files:
+    os.rename(f'{TBOARD_CUR_DIR}/{file}', f'{TBOARD_HIST_DIR}/{file}')
+
+# check for save file
+if os.path.exists(SAVE_FILE):
+    print('    found save file. setting LOAD to True')
+    LOAD = True
+print('--done with file management--')
 
 # import data
 if DEBUG + 'load_data':
@@ -106,9 +117,10 @@ if DEBUG + 'load_data':
     train_labels = to_categorical(train_labels, 2).astype(int)
     test_imgs = test_imgs.reshape((-1, 110, 110, 1))
     test_labels = to_categorical(test_labels, 2).astype(int)
+    act_collec_imgs = test_imgs[0:5]
 else:
     print('skipping data load because of debug')
-    test_labels = test_imgs = train_labels = train_imgs = None
+    act_collec_imgs = test_labels = test_imgs = train_labels = train_imgs = None
 
 if LOAD:
     print('    loading model')
@@ -118,17 +130,17 @@ else:
     model = k.Sequential()
 
     model.add(Conv2D(16, (3, 3), padding='same', activation='relu', input_shape=(110, 110, 1), name='m1.0'))
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', name='m1.1'))
     model.add(MaxPool2D(pool_size=(2, 2)))  # (110, 110) -> (55, 55)
     model.add(Dropout(0.15))
 
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', name='m2.0'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', name='m2.1'))
     model.add(MaxPool2D(pool_size=(2, 2)))  # (55, 55) -> (?, ?)
     # model.add(Dropout(0.25))
 
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', name='m3.0'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', name='m3.1'))
     model.add(MaxPool2D(pool_size=(2, 2)))  # (55, 55) -> (?, ?)
     # model.add(Dropout(0.25))
 
@@ -157,26 +169,33 @@ class ActivationCollector(k.callbacks.Callback):
         print('defining image collector for intermediate layers')
         self.data = data
         self.images = dict()
-        self.names = list()
-        for layer in self.model.layers:
-            self.names.append(layer.name)
-        for name in layer_names:
-            if name in self.names:
-                self.images[name] = list()  # add layer names to images if layer from model  has name
+        self.model_names = list()
+        self.mid_stages = None
+        self.layer_names = layer_names
+
+    def on_train_begin(self, logs=None):
+        self.model_names = [layer.name for layer in self.model.layers]
+        for name in self.layer_names:
+            if name in self.model_names:
+                # add layer names to images if layer from model as np.ndarray with shape layer.output_shape
+                layer_shape = self.model.get_layer(name).output_shape
+                self.images[name] = np.ndarray((1,) + layer_shape[1:])
             else:
                 print(f'    could not find layer with name {name}')
         self.mid_stages = k.Model(self.model.inputs,
-                                  [self.model.get_layer(name).outputs for name in list(self.images.keys())])
+                                  [self.model.get_layer(name).output for name in list(self.images.keys())])
 
-    def on_batch_end(self, batch, logs=None):
-        if logs is None:
-            return
-
+    def on_epoch_end(self, epoch, logs=None):
+        preds = self.mid_stages.predict(self.data)  # [layers, batch, width, height, channels]
+        for i, key in enumerate(self.images.keys()):
+            self.images[key] = np.concatenate((self.images[key], preds[i]))
 
 
 callbacks = list()
 callbacks.append(k.callbacks.TensorBoard(log_dir=TBOARD_CUR_DIR, histogram_freq=1, batch_size=32,
                                          write_graph=True, write_grads=True, write_images=True))
+act_collector = ActivationCollector(['m1.0', 'm1.1', 'm2.0', 'm2.1', 'm3.0', 'm3.1'], act_collec_imgs)
+callbacks.append(act_collector)
 
 if SAVE:
     callbacks.append(k.callbacks.ModelCheckpoint(SAVE_FILE, monitor='val_loss', save_best_only=True, mode='min'))
@@ -185,7 +204,7 @@ if SAVE:
 if DEBUG + 'train':
     print('training model')
 
-    model.fit(train_imgs, train_labels, validation_data=(test_imgs, test_labels), epochs=20, batch_size=32,
+    model.fit(train_imgs, train_labels, validation_data=(test_imgs, test_labels), epochs=20, batch_size=64,
               shuffle=True,
               callbacks=callbacks)
 
