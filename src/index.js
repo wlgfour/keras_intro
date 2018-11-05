ad577a10640762fc3c783b564e10991315663426
Will Gerecke <wlgfour@gmail.com>
2018-11-05 13:53:03 -0500

    fixed bug in ActivationCollector and seperated imgs to divs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+function check_file_extension(file_name, valid_extensions) {
+    let ret = false;
+    for(let i = 0; i < valid_extensions.length; i++) if (file_name.indexOf(valid_extensions[i]) > -1) {
+        ret = true;  // if one extension if in the string
+    }
+    return ret;
+}
+
+let valid_extensions = ['.gif', '.png', '.jpg'];
+
 $(document).ready(function() {
     let srcs = '';
     $.ajax({
@@ -9,17 +19,23 @@ $(document).ready(function() {
         }
         });
     srcs = srcs.split('\n');
-    for(let i = 0; i < srcs.length; i ++) {
-        if(srcs[i].indexOf('START::') > -1) {
-            $('body').append(`<h3>${srcs[i].split('::')[1]}</h3>`)
+    let $div = [];
+    for(let i = srcs.length - 1; i >= 0; i --) {
+        if(srcs[i].indexOf('START::') > -1) {  // start and stop are switched in meaning when array reversed
+            $div.append('<div class="hr"></div>');
+            $('body').append($div);
         }else if(srcs[i].indexOf('END::') > -1) {
-            $('body').append('<div class="hr"></div>')
-        }else {
+            $div = $('<div class="section-head"></div>');
+            $div.append(`<h3>${srcs[i].split('::')[1]}</h3>`);
+        }else if(check_file_extension(srcs[i], valid_extensions)) {
             let temp_div = $('<div></div>');
             let file_name = srcs[i].split('/');
             temp_div.append(`<h5>${file_name[file_name.length - 1]}</h5>`);
             temp_div.append(`<img src="${srcs[i]}">`);
-            $('body').append(temp_div);
+            $div.append(temp_div);
         }
     }
 });
+
+// &laquo;
+// &raquo;
