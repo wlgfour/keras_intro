5216bf18d4cf2c2427a51dcb444b8184a6f37af9
Will Gerecke <wlgfour@gmail.com>
2018-11-08 10:50:24 -0500

    added a new page for live_predictions. Oops, wrong branch

---
+++ src/live_pred.js
@@ -0,0 +1,141 @@
+// WARNING: this is bad code. Read at your own risk
+
+
+let model;
+let points = Array(30);
+
+class Img {
+    constructor(image, max) {
+        this.img = image;
+        this.max = max;
+    }
+}
+
+
+//--------------- BEGIN video functions -----------------------
+
+function start_video(stream) {
+    const video = document.querySelector('#stream video');
+    video.src = window.URL.createObjectURL(stream);
+    // video.play();
+}
+
+function stop_video(stream) {
+    const video = document.querySelector('#stream video');
+    video.parentNode.removeChild(video);
+    video.src = window.URL.revokeObjectURL(stream);
+}
+
+function capture() {
+    const canvas = document.querySelector('#grid_canvas');
+
+    const video_el = document.querySelector('#stream video');
+    canvas.width = 96;
+    canvas.height = 96;
+
+    // TODO: priority: high -- crop image rather than scale. webcam gets image not square
+    let context = canvas.getContext('2d');
+    context.drawImage(video_el, 0, 0, 96, 96);
+
+    let image = context.getImageData(0, 0, 96, 96);
+    let avg = 0;
+    for(let i = 0; i < image.data.length; i+=4) {
+        avg = (image.data[i] + image.data[i + 1] + image.data[i + 2]) / 3;
+        image.data[i] = avg;
+        image.data[i + 1] = avg;
+        image.data[i + 2] = avg;
+        image.data[i + 3] = 255;
+    }
+    context.putImageData(image, 0, 0);
+}
+
+function init_video() {
+    // begin video streaming
+    // reference: https://www.jonathan-petitcolas.com/2016/08/24/taking-picture-from-webcam-using-canvas.html
+    let btn = document.getElementById('init_video_btn');
+    btn.parentNode.removeChild(btn);
+    getUserMedia({
+        video: true,
+        audio: false,
+        width: 640,
+        height: 480,
+        el: 'stream'  // render live video in #stream
+    }, stream => {
+        start_video(stream);
+        document.getElementById('capture').addEventListener('click', () => {
+            capture();
+            // stop_video(stream);
+        })
+    }, err => console.error(err));
+}
+
+function preprocess_resize(width, height, image_data) {
+    // image_data = context.getImageData().data
+    let new_data = [];
+    let max = 0;
+    // get every 4th element -- inefficient as it is a separate loop
+    for(let i = 0; i < 96*96; i++) {
+        new_data[i] = image_data[i*4];
+        if(image_data[i*4] > max) {
+            max = image_data[i*4];
+        }
+    }
+    let data_resize = [];
+    let temp = [];
+    let pix = 0;
+    let ttemp = [];
+    for(let i = 0; i < 96; i++) {
+        temp.length = 0;
+        for(let k = 0; k < 96; k++) {
+            ttemp = [];
+            ttemp[0] = new_data[pix] / max;
+            temp[k] = ttemp;
+            pix++;
+        }
+        data_resize[i] = temp;
+    }
+    return new Img(data_resize, max);
+}
+
+async function make_prediction_promise() {
+    let img_el = document.getElementById('grid_canvas');
+    let context = img_el.getContext('2d');
+    let image = context.getImageData(0, 0, 96, 96);
+    let data = image.data;
+    let img = preprocess_resize(96, 96, data);
+    return await model.predict(tf.tensor([img.img])).data();
+}
+
+function make_prediction() {
+    const canvas_large = document.querySelector('#canvas_large');
+    let context_large = canvas_large.getContext('2d');
+    canvas_large.width = 680;
+    canvas_large.height = 680;
+    context_large.fillStyle = 'rgb(200,0,0)';
+    capture();
+    const video_el = document.querySelector('#stream video');
+    make_prediction_promise().then(function(values) {
+        points = values;
+        make_prediction();
+    });
+    context_large.drawImage(video_el, 0, 0, 680, 680);
+    for(let i = 0; i < points.length; i+=2) {
+        // context_large.fillRect(values[i] * 680, values[i + 1] * 680, 5, 5);
+        // console.log(values[i], values[i + 1]);
+        context_large.fillRect(points[i] * 680, points[i + 1] * 680, 5, 5);
+    }
+}
+
+async function load_model(path) {
+    // let btn = document.getElementById('load_model');
+    // btn.parentNode.removeChild(btn);
+    model = await tf.loadModel(path);
+}
+
+// ----------------------- END video functions ------------------
+
+$(document).ready(function() {
+    // -------------- video implementation --------------
+    init_video();
+    // load_model('keras_face_landmarks/log_dir/v1.1/face_keypoints_v1.1_js/model.json');
+});
