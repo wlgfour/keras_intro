549fecadafb8b9f6c1942042ec351b6d3e09d3bf
Will Gerecke <wlgfour@gmail.com>
2018-12-07 01:12:52 -0500

    added ZoomOut layer for ImAug image augmentation model. Outputs are in an odd order, but they will be flattened anyways so it doesn't matter. TensorFlow is a big pain

--- keras_face_landmarks/ImageAugmentation.py
+++ keras_face_landmarks/ImageAugmentation.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Layer
 
-
 """
 Goal:
     1) preprocess a random selection of images to be augmented
@@ -36,6 +35,7 @@ class RandomCrop(Layer):
         cropped the same as images
     !!! - will output shape (batch, len(ratios), height, width, channels)
           because model.predict only supports batch size equal to input batch size
+    !!! - interesting bug when passed only one image rather than an array of images
     supports:
         - list of ratios in (0, 1) - 1/2 will be half the original image resize to original (i.e. 2x zoom)
         - output size
@@ -93,14 +93,113 @@ class RandomCrop(Layer):
         return self.crop_resize
 
     def compute_output_shape(self, input_shape):
-        return tf.TensorShape([tf.constant(-1), tf.constant(self.output_dim[0]),
-                               tf.constant(self.output_dim[1]), input_shape[-1]])
+        return tf.TensorShape([tf.constant(-1), tf.constant(self.perms), tf.constant(self.output_dim[0]),
+                               tf.constant(self.output_dim[1]), input_shape[-1]])  # (batch, perms, height, ...)
+
+
+class ZoomOut(Layer):
+    """
+    zooms out of an image with various padding options
+    steps:
+        inputs: output_shape(height, width), shrink_factor, padding('CONSTANT', 'REFLECT', 'SYMMETRIC'), constant=0.
+        -) calculate new image shapes by shrink_factor * dimension: shape=(len(shrink_factor), 2(height, width))
+        -) resize to new dimensions. changes shape to (batch, len(shrink_factor), height, ...)
+        -) find number of items to pad on top and bottom. output_dimension - new_dimension
+        -) find random split for top vs. bottom pad. splits = random(0, num_pads)
+        -) pad images s.t. side-1-pad = split, side-2-pad = number-of-elems-to-pad - split
+    """
+
+    def __init__(self, output_shape, shrink_factors, padding='SYMMETRIC', constant=0, **kwargs):
+        super(ZoomOut, self).__init__(**kwargs)
+        self.out_shape = output_shape  # [height, width]
+        self.out_shape_tensor = tf.constant(output_shape)
+        self.shrink_factors = shrink_factors
+        self.shrink_factors_tensor = tf.constant(shrink_factors)
+        self.perms = len(shrink_factors)
+        self.padding = padding
+        self.constant = constant
+        self.in_shape = None
+
+    def build(self, input_shape):
+        super(ZoomOut, self).build(input_shape)
+        self.in_shape = input_shape
+
+    def call(self, inputs, **kwargs):
+        in_shape = tf.shape(inputs)  # has to have shape (batch, height, width, channels)
+        w_h = in_shape[-3:-1]
+        new_w_h = tf.map_fn(lambda r: tf.scalar_mul(r, tf.cast(w_h, tf.float32)),
+                            self.shrink_factors_tensor, name='new_w_h')
+        new_w_h = tf.cast(new_w_h, tf.int32)  # shape=(self.perms, 2)
+        # imgs_small = tf.map_fn(lambda wh: tf.image.resize_images(inputs, tf.cast(wh, tf.int32), align_corners=True),
+        #                        tf.cast(new_w_h, tf.float32), name='small_images')
+        # shape=(perms, batch, height, ...). can't reshape because some images got cropped to different sizes
+        total_pad_elems = tf.map_fn(lambda wh: self.out_shape_tensor - wh, new_w_h, name='total_pad_elems')
+        # shape should = (self.perms, 2)
+        # !!! - WARNING: really only good for square images
+        splits = tf.map_fn(lambda tot_pad: tf.cond(tf.logical_or(tf.equal(tot_pad[0], tf.constant(0)),
+                                                                 tf.equal(tot_pad[1], tf.constant(0))),
+                                                   lambda: tf.constant([0, 0]),
+                                                   lambda: tf.concat(
+                                                       [tf.random_uniform((1,), tf.constant(0), tot_pad[0], tf.int32),
+                                                        tf.random_uniform((1,), tf.constant(0), tot_pad[1], tf.int32)],
+                                                       axis=0)
+                                                   ), total_pad_elems, name='pad_split')
+        # generates a tensor of shape=(perms, 2) with random values of where to split the pads for top and bottom
+        split_and_tot = tf.stack([splits, total_pad_elems], axis=1)
+        pad_lens = tf.map_fn(lambda spl_tot: tf.stack([spl_tot[0], spl_tot[1] - spl_tot[0]], axis=0),
+                             split_and_tot, name='width_height_l_r_pads')
+        # creates a list of [[pad_elems_vertical_left, "horizontal__left], ["_right, "_right]]
+        # below: while loop to pad images to return size
+        k = tf.constant(0)
+        condition = lambda _i, _: tf.less(_i, tf.constant(self.perms), name='pad_while_cond')  # i < perms
+        # padded0 = tf.zeros(tf.TensorShape([1, self.in_shape[0].value, self.out_shape[0],
+        #                                    self.out_shape[1], self.in_shape[-1].value]),
+        #                    dtype=tf.int32, name='padded_imgs')
+        padded0 = tf.zeros([tf.constant(1), in_shape[0], self.out_shape_tensor[0],
+                            self.out_shape_tensor[1], self.in_shape[-1]])
+
+        # tf.expand_dims(tf.reshape(tf.pad(imgs_small[0], [[0, 0], [pad_lens[0][0][0], pad_lens[0][1][0]],
+        #                                                 [pad_lens[0][0][1], pad_lens[0][1][1]], [0, 0]],
+        #                                 mode=self.padding, constant_values=self.constant,
+        #                                 name='pad_imgs_to_out_shape0'),
+        #                          [in_shape[0], self.out_shape_tensor[0],
+        #                           self.out_shape_tensor[1], self.in_shape[-1]]
+        #                          ), 0)
+
+        def body(_i, _padded):
+            img_small = tf.image.resize_images(inputs, new_w_h[_i], align_corners=True)
+            __padded = tf.expand_dims(tf.pad(img_small, [[0, 0], [pad_lens[_i][0][0], pad_lens[_i][1][0]],
+                                                         [pad_lens[_i][0][1], pad_lens[_i][1][1]], [0, 0]],
+                                             mode=self.padding, constant_values=self.constant,
+                                             name='pad_imgs_to_out_shape'), axis=0)
+            ret = tf.concat([__padded, _padded], axis=0, name='padded_img')
+            return tf.add(_i, 1), ret
+
+        padded = tf.while_loop(condition, body, [k, padded0],
+                               shape_invariants=[k.get_shape(), tf.TensorShape([None,
+                                                                                self.in_shape[0].value,
+                                                                                self.out_shape[0],
+                                                                                self.out_shape[1],
+                                                                                self.in_shape[-1].value])],
+                               name='pad_while_loop')[1]
+        # padded = tf.map_fn(lambda im_pad: tf.pad(im_pad[0], [[0, 0], [im_pad[1][0][0], im_pad[1][1][0]],
+        #                                                [im_pad[1][0][1], im_pad[1][1][1]], [0, 0]],
+        #                                           mode=self.padding, constant_values=self.constant,
+        #                                           name='pad_imgs_to_out_shape0'), (imgs_small, pad_lens))
+        output_shape_cast = [self.in_shape[0].value, self.perms, self.out_shape[0],
+                             self.out_shape[1], self.in_shape[-1].value]
+        output_shape_cast = [-1 if _el is None else _el for _el in output_shape_cast]  # None -> -1 for tensor
+        return tf.reshape(padded[1:], output_shape_cast)
+
+    def compute_output_shape(self, input_shape):
+        return tf.TensorShape([tf.constant(-1), tf.constant(self.perms), tf.constant(self.out_shape_tensor[0]),
+                               tf.constant(self.out_shape_tensor[1]), input_shape[-1]])
 
 
 class ImAug(keras.Model):
     """class that inherits keras.model and creates an image augmentation model that outputs"""
 
-    def __init__(self, output_shape, ratios=None):
+    def __init__(self, output_shape, ratios=None, shrink_factors=None):
         """
 
         :param output_shape: has shape [output_height, output_width]
@@ -109,17 +208,21 @@ class ImAug(keras.Model):
         super(ImAug, self).__init__()
         self.point_layers = []
         if ratios is not None:
-            self.point_layers.append(RandomCrop(ratios, output_shape))
+            lyr = RandomCrop(ratios, output_shape)
+            self.point_layers.append(lyr)
+        if shrink_factors is not None:
+            lyr = ZoomOut(output_shape, shrink_factors)
+            self.point_layers.append(lyr)
 
     def call(self, inputs, training=None, mask=None):
         # https://www.tensorflow.org/api_docs/python/tf/keras/models/Model
         # define forward pass of the network
-        output = None
+        outputs = []
         p_layer = inputs
         for layer in self.point_layers:
-            output = layer(p_layer)
-            p_layer = layer
-        return output
+            outputs.append(layer(p_layer))
+            # p_layer = layer
+        return tf.concat(outputs, axis=1)
 
 
 if __name__ == '__main__':
@@ -145,10 +248,10 @@ if __name__ == '__main__':
     plt.imshow(train_data[0, :, :, 0])
     plt.show()
 
-    model = ImAug([96, 96], ratios=[0.5, 0.75, 1.])
+    model = ImAug([96, 96], ratios=[0.5, 0.75, 1.], shrink_factors=[0.5, 0.75, 1.])
     # (96, 96)
-    out = model.predict(train_data[0:1])
-    for i in range(3):
+    out = model.predict(train_data[0:3])
+    for i in range(len(out[0])):
         plt.imshow(out[0, i, :, :, 0])
         plt.show()
     model.summary()
