1ef350d1712ee9d1bd809fbb0d7a6b40784b88da
Will Gerecke <wlgfour@gmail.com>
2018-12-08 15:42:00 -0500

    added a stable copy of ImageAugmentation with no layer operations(extracting x-y coords) to the Helpers package

--- Helpers/ImageAugmentation.py
+++ Helpers/ImageAugmentation.py
@@ -7,6 +7,7 @@ import tensorflow.keras as keras
 from tensorflow.keras.layers import Layer
 
 """
+!!! This is a stable version of the ImageAugmentation present in keras_face_landmarks without the __main__ block
 Goal:
     1) preprocess a random selection of images to be augmented
     2) operations 
@@ -280,41 +281,3 @@ class ImAug:
         - wraps self.model.summary
         """
         self.point_model.summary()
-
-
-if __name__ == '__main__':
-    f = np.load('data/face_images.npz')
-    data = f['face_images']
-    f.close()
-    new_data = list()
-    for i in range(np.shape(data)[2]):
-        new_data.append(data[:, :, i])
-    data = np.array(new_data)  # change from (height, width, images) to (images, height, width)
-    df = pd.read_csv('data/facial_keypoints.csv')
-    df = df.reset_index()  # makes index df[image][0]
-    # df.fill_na(0)  # fill nan with 0
-    # TODO: fill rather than drop nan. do not increase loss for points with true values of 0 -- custom loss
-    df = df.dropna()  # easy way out. lots of data lost
-    pnts = df.values  # df to np.ndarray shape=(imgs, 30)
-    data = data[pnts[:, 0].astype('int32')]  # get all images that haven't been dropped
-    train_data = data * (1 / data.max())  # scale to [0, 1]
-    train_data = train_data.reshape((-1, 96, 96, 1))
-    train_labels = pnts[:, 1:]
-    train_labels = train_labels * (1 / np.shape(data)[1])  # scale so scaled * image_width = original position
-
-    plt.imshow(train_data[0, :, :, 0])
-    plt.show()
-
-    model = ImAug([96, 96, 1], [48, 48, 1], ratios=[0.5, 0.75, 1.], shrink_factors=[0.5, 0.75, 1.], pad_mode='CONSTANT')
-    out_layer, model_in = model.get_output_layer()
-    model2 = ImAug([48, 48, 1], [72, 72, 1], inputs=out_layer, model_input=model_in,
-                   ratios=[0.5, 0.75, 1.], shrink_factors=[0.1])
-    # (96, 96)
-    keras.utils.plot_model(model2.point_model, 'ImAug_chain.png', show_shapes=True)
-    model2.summary()
-    out = model(train_data[0:5])
-    for i, img in enumerate(out[-1::-1]):
-        plt.imshow(img[:, :, 0])
-        plt.show()
-        if i > 10:
-            break
--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -2,3 +2,4 @@ from Helpers.ImageHandler import ImageHandler
 from Helpers.ActivationCollector import ActivationCollector
 from Helpers.Debug import Debug
 from Helpers.FileArchitecture import FileArchitecture
+from Helpers.ImageAugmentation import MyReshape, RandomCrop, ZoomOut, ImAug
